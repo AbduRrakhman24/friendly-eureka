c573263f733a774a9d3d8c769a56a5c29c3e2950
AbduRrkhman24 <ahmadov.1999@bk.ru>
2023-08-22 21:15:44 +0300

    commit - 5

--- AbduRrakhman,,,idcoding/main2.js
+++ AbduRrakhman,,,idcoding/main2.js
@@ -1,14 +1,53 @@
 
 let a = [];
 
-debugger;
 
-for (let i = 0; i <= 1000; i+=2){
-    a[i] = i + 2;
-    if (a[i] % 3) {
-        let b = [];
+for (let i = 1; i <= 1000; i++)
+{
+    if (i % 2 === 0) 
+    {
+        a[i] = i;
     };
-    else ()
-    a[i] = a[i] * a[i];
-
+    
 }
+
+
+let b = [];
+
+
+for (let i = 0; i < a.length;  i++)
+{
+   let ByThree = a[i] % 3;
+    if (isNaN(ByThree)){
+        continue;
+    }
+    if (ByThree > 0){
+        continue;
+    }
+    if(ByThree === 0){
+        b[i] = i;
+    }
+}
+console.log(b);
+let c = [];
+
+
+
+for (let i = 0; i < b.length; i++)
+{
+    let q = b[i] * b[i];
+    if (isNaN(q)){
+        continue;
+    }
+    c[i] = q;
+    
+}
+console.log(c);
+
+let e = 0;
+for (i = 0; i < c.length; i++){
+    if (c[i] === 324) {
+        e = i;   
+    }
+}
+console.log(e);
